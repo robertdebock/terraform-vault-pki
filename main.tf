366655d2ed9fe82cf887ba6239b23b028a3edd7b
Robert de Bock <robert@meinit.nl>
2022-05-20 10:09:49 +0200

    Set the lease-times more realistic, setup crl location and config and store certificates.

--- main.tf
+++ main.tf
@@ -29,10 +29,11 @@ resource "tls_self_signed_cert" "ca_cert" {
 resource "vault_mount" "root" {
   path                      = "pki"
   type                      = "pki"
-  default_lease_ttl_seconds = 3600
+  default_lease_ttl_seconds = 315360000 # 10 years
   max_lease_ttl_seconds     = 315360000 # 10 years
 }
 
+# Configure the root-CA with the bundle containing both private key and certificate.
 resource "vault_pki_secret_backend_config_ca" "ca_config" {
   backend    = vault_mount.root.path
   pem_bundle = join("", [tls_private_key.ca_key.private_key_pem, tls_self_signed_cert.ca_cert.cert_pem])
@@ -42,8 +43,26 @@ resource "vault_pki_secret_backend_config_ca" "ca_config" {
 resource "vault_mount" "intermediate" {
   path                      = "pki-intermediate"
   type                      = "pki"
-  default_lease_ttl_seconds = 3600
-  max_lease_ttl_seconds     = 315360000 # 10 years
+  default_lease_ttl_seconds = 157680000 # 5 years
+  max_lease_ttl_seconds     = 157680000 # 5 years
+}
+
+# Configure a certificate revokation list
+resource "vault_pki_secret_backend_crl_config" "crl_config" {
+  backend = vault_mount.intermediate.path
+  expiry  = "72h"
+  disable = false
+}
+
+# Configure the URLS for certificates and certificate revokation lists.
+resource "vault_pki_secret_backend_config_urls" "example" {
+  backend = vault_mount.root.path
+  issuing_certificates = [
+    "http://127.0.0.1:8200/v1/pki/crt",
+  ]
+  crl_distribution_points = [
+    "http://127.0.0.1:8200/v1/pki/crl"
+  ]
 }
 
 # Create a certificate signing request for the intermedate-CA.
@@ -83,6 +102,7 @@ resource "vault_pki_secret_backend_role" "default" {
   allow_glob_domains = false
   allow_any_name     = false
   enforce_hostnames  = true
+  generate_lease     = true
   allow_ip_sans      = true
   server_flag        = true
   client_flag        = false
@@ -90,7 +110,6 @@ resource "vault_pki_secret_backend_role" "default" {
   organization       = ["Adfinis"]
   country            = ["NL"]
   locality           = ["Overijssel"]
-  max_ttl = 63113904
-  ttl      = 2592000
-  no_store = true
+  max_ttl            = 63113904
+  ttl                = 2592000
 }
